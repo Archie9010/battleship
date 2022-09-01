2049574aa68f53e5e46c3722c5c8f7daf7e52c44
Archie9010 <archie9010@protonmail.com>
2022-09-01 10:19:58 +0000

    Add initial board print text

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+board = []
+
+# The boar
+for x in range(7):
+    board.append(["O"]*7)
+
+def print_board(board):
+    for row in board:
+        print("".join(row))
+
+
+# Initial board print
+print ("Let's play Battleship!  There are 2 ships on the field.  Can you find one?")
+print_board(board)                          
