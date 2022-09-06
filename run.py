8f484be5c11f794766d0447f359019d04501b122
Archie9010 <archie9010@protonmail.com>
2022-09-06 08:44:35 +0000

    Add update to the code

--- run.py
+++ run.py
@@ -6,21 +6,24 @@ board = []
 for x in range(5):
     board.append(["O"]*5)
 
+
 def print_board(board):
     for row in board:
         print("".join(row))
 
 
 # Initial board print
-print ("Let's play Battleship!  There are 2 ships on the field. Can you find one?")
-print_board(board)                          
+print("Let's play Battleship!There are 2 ships on the field. Can you find one?")
+print_board(board)
+
 
 # Calculate random row
 def random_row(board):
-    return randint(0, len(board) -1)
+    return randint(0, len(board)-1)
+
 
 def random_col(board):
-    return randint(0, len(board) -1)
+    return randint(0, len(board)-1)
 
 # Place and print ship 1 .
 ship_row = random_row(board)
@@ -28,17 +31,15 @@ ship_col = random_col(board)
 shiplist1 = [ship_row, ship_col]
 
 # Take input for row and column
-for turn in range(1,10):
-    print("Turn", turn +1)
+for turn in range(1, 10):
+    print ("Turn", turn+1)
     guess_row = int(input("Guess Row 1-5:"))-1
     guess_col = int(input("Guess Col 1-5:"))-1
-     
 # Win statement
     if (guess_row == ship_row and guess_col == ship_col) or (guess_row == ship_row2 and guess_col == ship_col2):
         print("Congratulation! You sunk my battleship!")
         break
- 
-# Out of boundaries and repeat guess 
+# Out of boundaries and repeat guess
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print("That's not even in the water. Try again!")
@@ -48,7 +49,7 @@ for turn in range(1,10):
         else:
             print("You missed Ship!")
             board[guess_row][guess_col] = "X"
-        if turn == 9: 
-            print("Game Over")   
+        if turn == 9:
+            print("Game Over")
 # Print board with X's
-    print_board(board)
+    print_board(board) 
